c2ef4c0a5d20576143f23198d6af4b2da53f523c
Elena Shagaeva <30930203+tommiknocker@users.noreply.github.com>
2017-10-25 02:24:53 +0300

    закончена стилизация

--- js/script.js
+++ js/script.js
@@ -2,7 +2,18 @@
 
   var writeUs = document.querySelector(".contact-us-btn");
 	var openForm = document.querySelector(".modal-form");
+  var closeForm = openForm.querySelector(".modal-form-close");
+  var login = openForm.querySelector("[name=username]");
 
 	writeUs.addEventListener("click", function (evt) {
 	evt.preventDefault();
-	openForm.classList.toggle("form-show");});
+	openForm.classList.add("form-show");
+  login.focus();});
+
+  closeForm.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  openForm.classList.remove("form-show");});
+
+  window.addEventListener("keydown", function (evt) {
+  if (evt.keyCode === 27) {
+  openForm.classList.remove("form-show");}   });
